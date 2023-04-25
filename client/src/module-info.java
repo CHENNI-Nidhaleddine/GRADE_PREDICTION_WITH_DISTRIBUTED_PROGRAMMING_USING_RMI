89a998d62cb06b4dfbaafd5f8aec3713181d2c8d
unknown <ne.chenni@gmail.com>
2023-04-25 02:13:36 +0100

    final version

--- client/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module GUI_IRIAD {
-	requires javafx.controls;
-	requires java.rmi;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
--- server/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module Serveur_IRIAD {
-	requires javafx.controls;
-	requires java.rmi;
-	requires commons.math3;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	exports application;
-}
